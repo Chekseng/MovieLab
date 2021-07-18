35dfc9a845a2e3f0c24215af53fea2ccd66644cd
Chekseng <76708577+Chekseng@users.noreply.github.com>
2021-07-18 16:45:35 +0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
-  ],
-  devServer: {
-    disableHostCheck: true,
-  }
+    '@vue/cli-plugin-babel/preset',
+      devServer: {
+        disableHostCheck: true,
+      }
+  ]
 }
